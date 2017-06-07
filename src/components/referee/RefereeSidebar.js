aba6e0bb4524e4abec638c99b27c1b535023caa2
Cameron Aziz <cameronaziz@gmail.com>
2017-06-06 18:06:11 -0700

    cleanup

--- src/components/referee/RefereeSidebar.js
+++ src/components/referee/RefereeSidebar.js
@@ -16,29 +16,13 @@ class RefereeSidebar extends Component {
     return(
         <nav className="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
           <ul className="nav nav-pills flex-column">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">Overview <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Reports</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Analytics</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Export</a>
-            </li>
-          </ul>
-          <ul className="nav nav-pills flex-column">
             {this.lineItem('/referee/teams', 'Teams')}
-
           </ul>
-
           <ul className="nav nav-pills flex-column">
             {this.lineItem('/referee/sports', 'Sports')}
           </ul>
         </nav>
-    )
+    );
   }
 }
 export default RefereeSidebar;
