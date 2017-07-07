d6721bef6493f733b00cfc71085f78ba2becf731
Cameron Aziz <cameronaziz@gmail.com>
2017-07-07 14:32:59 -0700

    Locations

--- src/actions/locations.js
+++ src/actions/locations.js
@@ -2,12 +2,24 @@ import config from '../lib/config';
 import * as actionTypes from './actionTypes';
 import * as firebase from '../lib/firebase';
 
-let ref = firebase.db.ref('sports');
+let ref = firebase.db.ref('locations');
 
 export function loadLocations() {
   return function(dispatch) {
     ref.on('value', function (snapshot) {
       dispatch(loadLocationsSuccess(Object.values(snapshot.val())));
+    });
+  };
+}
+
+export function saveLocation(location) {
+  return function(dispatch) {
+    ref.push(location, function(error) {
+      if (error)
+        dispatch(createLocationSuccess(false));
+      else {
+        dispatch(createLocationSuccess(true));
+      }
     });
   };
 }
