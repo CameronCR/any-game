3ae33a2a62f785885afe35071e0983cb224729c2
Cameron Aziz <cameronaziz@gmail.com>
2017-07-08 22:24:40 -0700

    Add typechecking to List

--- src/components/referee/common/List.js
+++ src/components/referee/common/List.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import ListPreview from './ListPreview';
 
@@ -24,5 +25,12 @@ class List extends Component {
     );
   }
 }
+
+List.propTypes = {
+  list: PropTypes.array.isRequired,
+  setItem: PropTypes.func.isRequired
+};
+
+
 
 export default List;
