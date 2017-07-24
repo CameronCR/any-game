9308b8c1c257bc1fa5b89f407f9246ee072cfa92
Cameron Aziz <cameronaziz@gmail.com>
2017-07-23 17:11:07 -0700

    clean up

--- src/components/referee/games/GamesList.js
+++ src/components/referee/games/GamesList.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import ListPreview from './ListPreview';
-
 class List extends Component {
   constructor(props) {
     super(props);
